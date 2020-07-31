5dce2295c35733433b66ec026d673b58a2d11d7b
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-31 09:31:20 -0600

    Adds and passes existence

---
+++ lib/night_writer.rb
@@ -0,0 +1,7 @@
+class NightWriter
+
+ def initialize(message, braille)
+   
+ end
+
+end
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -5,7 +5,10 @@ require './lib/night_writer'
 class NightWriterTest < Minitest::Test
 
   def test_it_exists
-
+    messge = File.open("message.txt")
+    braille = File.open("braille.txt")
+    night_writer = NightWriter.new(message, braille)
+    assert_instance_of NightWriter, night_writer
   end
 
-end 
+end
