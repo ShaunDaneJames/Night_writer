9da33ed69e43f4223725cae89a4cf650eef7c9f9
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-31 15:02:57 -0600

    Adds read class and test

---
+++ lib/read_message.rb
@@ -0,0 +1,6 @@
+class ReadMessage
+
+
+
+
+end 
---
+++ test/read_message_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/read_message'
+
+class ReadMessageTest < Minitest::Test
+
+  def test_it_exists
+    message = File.open("message.txt")
+    braille = File.open("braille.txt")
+    read = ReadMessage.new(message, braille)
+    assert_instance_of ReadMessage, read
+  end
+
+end
