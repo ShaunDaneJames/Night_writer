37acd865ce84179608476118c92f0ce9899dbb41
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-01 14:02:22 -0600

    Now procudes accruate number of characters in text file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,12 +1,15 @@
 require_relative 'english_keys'
+require_relative 'read_message'
 
 class NightWriter
   include EnglishKeys
 
   input_array = ARGV
+  english_message = File.read(ARGV[0])
+  ARGV[1] = File.open("braille.txt")
+# require "pry"; binding.pry
 
-  puts "Created #{ARGV[1]} containing 256 characters"
-  puts input_array.to_s
+  puts "Created #{ARGV[1]} containing #{english_message.split("").count} characters"
   # def initialize(message, braille)
   #   @message = message
   #   @braille = braille
