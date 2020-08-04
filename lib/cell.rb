1cdcbd20e3ca2a5aa0b08719854fb695456bfac2
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-03 18:49:12 -0600

    Adds make cell method and passes test

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,4 +1,17 @@
 class Cell
+  attr_reader :first_row,
+              :second_row,
+              :thrid_row
+              
+  def initialize(letter_data)
+    @first_row = letter_data[0]
+    @second_row = letter_data[1]
+    @thrid_row = letter_data[2]
+  end
 
-
-end 
+  def make_cell(letter_data)
+    "#{@first_row}\n" +
+    "#{@second_row}\n" +
+    "#{@thrid_row}"
+  end
+end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -5,7 +5,24 @@ require './lib/cell'
 class CellTest < Minitest::Test
 
   def test_it_exists
-    cell = Cell.new
+    cell = Cell.new([])
     assert_instance_of Cell, cell
+  end
+
+  def test_can_format_braille
+    a = ["0.", "..", ".."]
+    cell1 = Cell.new(a)
+    expected_a = "0.\n" +
+               "..\n" +
+               ".."
+    o = ["0.", ".0", "0."]
+    cell2 = Cell.new(o)
+    expected_o = "0.\n" +
+                 ".0\n" +
+                 "0."
+                 # require "pry"; binding.pry
+    assert_equal expected_a, cell1.make_cell(a)
+    assert_equal expected_o, cell2.make_cell(o)
+
   end
 end
